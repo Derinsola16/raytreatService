a584071588560881bf640f910eefb84347d2bf68
Derinsola16 <aderinsolaodusanya@gmail.com>
2021-07-14 15:33:29 +0100

    doing it

--- contact.php
+++ contact.php
@@ -39,50 +39,25 @@
 
   // echo $contact->send();
 
-  $errors = [];
-$errorMessage = '';
 
-if (!empty($_POST)) {
+
+
     $name = $_POST['name'];
     $email = $_POST['email'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
+    $toEmail = 'aderinsolaodusanya@gmail.com';
+    $emailSubject = $subject;
+    $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
 
-    if (empty($name)) {
-        $errors[] = 'Name is empty';
-    }
+    $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
+    $body = join(PHP_EOL, $bodyParagraphs);
 
-    if (empty($subject)) {
-      $errors[] = 'Subject is empty';
-  }
-
-    if (empty($email)) {
-        $errors[] = 'Email is empty';
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = 'Email is invalid';
-    }
-
-    if (empty($message)) {
-        $errors[] = 'Message is empty';
-    }
-
-
-    if (empty($errors)) {
-        $toEmail = 'aderinsolaodusanya@gmail.com';
-        $emailSubject = $subject;
-        $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
-
-        $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
-        $body = join(PHP_EOL, $bodyParagraphs);
-
-        if (mail($toEmail, $emailSubject, $body, $headers)) {
-            header('Location: thank-you.html');
-            echo('Here')
-        } else {
-            $errorMessage = 'Oops, something went wrong. Please try again later';
-        }
+    if (mail($toEmail, $emailSubject, $body, $headers)) {
+        // header('Location: thank-you.html');
+        echo 'Email Sent'
     } else {
-        $allErrors = join('<br/>', $errors);
-        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+        $errorMessage = 'Oops, something went wrong. Please try again later';
     }
+   
 ?>
