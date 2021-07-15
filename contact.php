931dd765c6c5c80e49c9a5e09b1608d35b54f864
Derinsola16 <aderinsolaodusanya@gmail.com>
2021-07-15 02:31:50 +0100

    omo

--- contact.php
+++ contact.php
@@ -48,7 +48,7 @@
     $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
 
     $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
-    $body = join(PHP_EOL, $bodyParagraphs);
+    // $body = join(PHP_EOL, $bodyParagraphs);
 
     if (mail($toEmail, $emailSubject, $body, $headers)) {
         // header('Location: thank-you.html');
